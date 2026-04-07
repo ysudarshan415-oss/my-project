e556811a0810d9447498ed23709a2a8ad94dda6d
shaurya yadav <ysudarshan415@gmail.com>
2026-04-07 13:55:50 +0530

    fix flake8 blank lines

--- app/app.py
+++ app/app.py
@@ -1,14 +1,10 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return jsonify(message="Hello DevOps Engg")
-
 @app.route("/health")
 def health():
     return jsonify(status="OK")
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
